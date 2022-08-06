be764cb3d21d26ed1ca81391a168fd1e698e3f1c
Fernando A. Zepeda Herrera <fazepher@gmail.com>
2022-08-07 00:24:18 +0100

    Pruebas iniciales de Metropolis-Hastings

---
+++ tests/testthat/test-Metropolis_Hastings.R
@@ -0,0 +1,63 @@
+test_that("Metropolis-Hastings step works", {
+
+  # We should always accept a same-place move
+  expect_identical(mh_step(x_curr = 0,
+                           x_prop = 0,
+                           l_curr = stats::dnorm(0, log = TRUE),
+                           l_prop = stats::dnorm(0, log = TRUE),
+                           lq_c2p = stats::dnorm(0, mean = 0, log = TRUE),
+                           lq_p2c = stats::dnorm(0, mean = 0, log = TRUE)),
+                   list(x_next = 0,
+                        l_next = stats::dnorm(0, log = TRUE),
+                        accepted = TRUE))
+
+  expect_identical(mh_step(x_curr = matrix(0, nrow = 5),
+                           x_prop = matrix(0, nrow = 5),
+                           l_curr = rep(stats::dnorm(0, log = TRUE), 5),
+                           l_prop = rep(stats::dnorm(0, log = TRUE), 5),
+                           lq_c2p = rep(stats::dnorm(0, mean = 0, log = TRUE), 5),
+                           lq_p2c = rep(stats::dnorm(0, mean = 0, log = TRUE), 5)),
+                   list(x_next = matrix(0, nrow = 5),
+                        l_next = rep(stats::dnorm(0, log = TRUE), 5),
+                        accepted = rep(TRUE, 5)))
+
+  # We should always accept an "up-hill" proposal
+  expect_identical(mh_step(x_curr = 1,
+                           x_prop = 0,
+                           l_curr = stats::dnorm(1, log = TRUE),
+                           l_prop = stats::dnorm(0, log = TRUE),
+                           lq_c2p = stats::dnorm(0, mean = 1, log = TRUE),
+                           lq_p2c = stats::dnorm(1, mean = 0, log = TRUE)),
+                   list(x_next = 0,
+                        l_next = stats::dnorm(0, log = TRUE),
+                        accepted = TRUE))
+  expect_identical(mh_step(x_curr = rep(1, 3),
+                           x_prop = rep(0, 3),
+                           l_curr = rep(stats::dnorm(1, log = TRUE), 3),
+                           l_prop = rep(stats::dnorm(0, log = TRUE), 3),
+                           lq_c2p = rep(stats::dnorm(0, mean = 1, log = TRUE), 3),
+                           lq_p2c = rep(stats::dnorm(1, mean = 0, log = TRUE), 3)),
+                   list(x_next = rep(0, 3),
+                        l_next = rep(stats::dnorm(0, log = TRUE), 3),
+                        accepted = rep(TRUE, 3)))
+
+  # We should reject with VERY HIGHT probability
+  expect_identical(mh_step(x_curr = 0,
+                           x_prop = 1000,
+                           l_curr = stats::dnorm(0, log = TRUE),
+                           l_prop = stats::dnorm(1000, log = TRUE)),
+                   list(x_next = 0,
+                        l_next = stats::dnorm(0, log = TRUE),
+                        accepted = FALSE))
+
+  # We should expect an acceptance rate of 25%
+  expect_equal(
+    replicate(5000,
+              mh_step(x_curr = 6, x_prop = 8,
+                      l_curr = log(0.5), l_prop = log(0.25),
+                      lq_c2p = log(0.7), lq_p2c = log(0.35))$accepted) |>
+      mean(),
+    0.25, tolerance = stats::qnorm(0.995)*sqrt(0.25*0.75/5000))
+
+
+})
