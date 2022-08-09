316dd737ed3e55f9cea662d97061751b0f1d5094
Fernando A. Zepeda Herrera <fazepher@gmail.com>
2022-08-09 02:36:47 +0100

    Continuamos distribuciones

--- R/distributions.R
+++ R/distributions.R
@@ -1,17 +1,81 @@
 
 
 # Basic Normal log densities and samplers
-lnorm <- function(x, mean = 0, sd = 1){
-  dnorm(x, mean, sd, log = TRUE)
-}
-rnorm <- stats::rnorm
 
-lnorm_temp <- function(x, beta = 1, mean = 0, sd = 1){
-  dnorm(x, mean, sd/sqrt(beta), log = TRUE)
-}
-rnorm_temp <- function(n, beta = 1, mean = 0, sd = 1){
-  rnorm(n, mean, sd/sqrt(beta))
-}
+#' Tempered Normal log densities and sampler
+#'
+#' These are basic wrappers around `stats::dnorm()` and `stats::rnorm()` to provide shortcut evaluation
+#' of the log-density of a univariate tempered normal as well as samples from it.
+#'
+#' Tempering a distribution means raising its density to a power \eqn{\beta>0},
+#' known as inverse temperature. A tempered normal centered at \eqn{\mu} with inverse temperature
+#' parameter \eqn{\beta} and standard deviation \eqn{\sigma} is equivalent to a normal
+#' with standard deviation \eqn{\sigma/\sqrt{\beta}} and the same mean parameter.
+#' This rescaling property can be seen by noticing that raising to a power means multiplying within
+#' the exponential term of the normal density and then renormalizing to integrate to 1.
+#'
+#'
+#' @param x vector of quantiles.
+#' @param mean vector of means.
+#' @param sd vector of standard deviations.
+#' @param beta inverse temperature parameter \eqn{\beta > 0}.
+#' @param n number of observations.
+#'
+#' @return
+#' `lnorm` gives the log-density of a normal without tempering (i.e. \eqn{\beta = 1}).
+#' On the other hand, the functions with suffix `_temp` do include the inverse temperature parameter,
+#' `beta`. The preffix `l` stands for log-density, `d` for density, and `r` for sampling.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' lnorm(x = 0)
+#' dnorm(x = 0, log = TRUE)
+#'
+#' dnorm_temp(x = 5, beta = 1)
+#' dnorm(x = 5)
+#'
+#' lnorm_temp(x = 0, beta = 0.5, sd = 1)
+#' lnorm(0, sd = 1/sqrt(0.5))
+#'
+#' rnorm_temp(n = 1000, mean = 100, sd = 1) |> hist()
+#' rnorm_temp(n = 1000, beta = 0.1, mean = 100, sd = 1) |> hist()
+#'
+#' # The functions inherit vectorization,
+#' # so can be used for example with ggplot2::stat_function()
+#' # to show the flattening effect of tempering
+#'
+#' ggplot() +
+#'   stat_function(fun = dnorm, color = "gray65") +
+#'   stat_function(fun = dnorm_temp, args = list(beta = 0.75), color = "steelblue4") +
+#'   stat_function(fun = dnorm_temp, args = list(beta = 0.5), color = "darkcyan") +
+#'   stat_function(fun = dnorm_temp, args = list(beta = 0.25), color = "blueviolet") +
+#'   stat_function(fun = dnorm_temp, args = list(beta = 0.25^2), color = "maroon4") +
+#'   xlim(-6,6) +
+#'   theme_classic()
+#'
+#'
+#'
+lnorm <- function(x, mean = 0, sd = 1){dnorm(x, mean, sd, log = TRUE)}
+
+#' @rdname lnorm
+#'
+#' @export
+#'
+dnorm_temp <- function(x, beta = 1, mean = 0, sd = 1) dnorm(x, mean, sd/sqrt(beta))
+
+#' @rdname lnorm
+#'
+#' @export
+#'
+lnorm_temp <- function(x, beta = 1, mean = 0, sd = 1) dnorm(x, mean, sd/sqrt(beta), log = TRUE)
+
+#' @rdname lnorm
+#'
+#' @export
+#'
+rnorm_temp <- function(n, beta = 1, mean = 0, sd = 1) rnorm(n, mean, sd/sqrt(beta))
 
 # Basic Multivariate normal log densities and samplers
 lmvtnorm_temp <- function(x, beta = 1, mu = 0, sigma = NULL,
