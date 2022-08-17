f6439c9b2a61566a88e9dfd11c0e5c99ac02771b
Fernando A. Zepeda Herrera <Fernando.Zepeda@warwick.ac.uk>
2022-08-17 13:50:35 +0100

    Corregimos bug de ciclos

--- R/HAT.R
+++ R/HAT.R
@@ -80,7 +80,7 @@ lHAT_target <- function(x, beta, HAT_info, ltemp_target, ..., G_type = 1, silent
 
 HAT_rwm_chain <- function(ltemp_target, ..., HAT_info, G_type = 1,
                           beta_schedule, swap_type = "deo",
-                          scale = 1, Temp_Moves = 1000, Within_Moves = 10,
+                          scale = 1, Cycles = 1000, Temp_Moves = 1, Within_Moves = 5,
                           x_0 = NULL, x_0_u = 2, seed = NULL,
                           custom_rw_sampler = NULL, target_names = NULL, d = NULL,
                           silent = FALSE){
@@ -89,7 +89,7 @@ HAT_rwm_chain <- function(ltemp_target, ..., HAT_info, G_type = 1,
   hat_args <- c(list(l_target = lHAT_target, G_type = G_type, HAT_info = HAT_info,
                      ltemp_target = ltemp_target), rlang::dots_list(...),
                 list(beta_schedule = beta_schedule, swap_type = swap_type,
-                     scale = scale, Temp_Moves = Temp_Moves, Within_Moves = Within_Moves,
+                     scale = scale, Cycles = Cycles, Temp_Moves = Temp_Moves, Within_Moves = Within_Moves,
                      x_0 = x_0, x_0_u = x_0_u, seed = seed,
                      custom_rw_sampler = custom_rw_sampler, target_names = target_names, d = d,
                      silent = silent))
