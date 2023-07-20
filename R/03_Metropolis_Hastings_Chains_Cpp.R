c4228f56be1eb1d412360fe3052551d69f18ce87
Fernando A. Zepeda Herrera <Fernando.Zepeda@warwick.ac.uk>
2023-07-20 11:51:26 +0100

    Metropolis Hastings Cpp

--- R/03_Metropolis_Hastings_Chains_Cpp.R
+++ R/03_Metropolis_Hastings_Chains_Cpp.R
@@ -104,3 +104,70 @@ rwm_global_scale_sampler_leaner_chain <- function(l_target, d, ...,
   return(list(x = x[,-(1:(burn+1))], l_x_curr = l_x, acc = acc[-seq(1,burn)], acc_rate = acc_rate))
 
 }
+
+mh_sampler_leaner_chain_cpp <- function(l_target, d, ...,
+                                        mh_sampler, other_sampler_args = NULL,
+                                        lq_mh, other_lq_args = NULL,
+                                        S = 1000, burn = 0,
+                                        x_0 = NULL, x_0_u = 2, l_0 = NULL,
+                                        seed = NULL, silent = FALSE){
+
+  #--- Preparation -------------
+
+  # Checking for valid sample sizes
+  stopifnot(S >= 1)
+  stopifnot(-1 <= burn && burn < S)
+
+  # Possibly set seed
+  if(!is.null(seed)){
+    set.seed(seed)
+  }
+
+  # Get starting point
+  if(is.null(x_0)){
+    stopifnot(is.numeric(x_0_u) && x_0_u > 0)
+    x_0 <- runif(d, min = -x_0_u, max = x_0_u)
+  }else{
+    stopifnot(is.numeric(x_0) && length(x_0) == d)
+  }
+
+  # Preallocate containers
+  x <- matrix(nrow = d, ncol = S + 1)
+  acc <- logical(S)
+
+  #--- Algorithm -------------
+
+  # Initialize
+  x[, 1] <- x_0
+  l_x <- l_0 %||% l_target(x_0, ...)
+
+  # Run iterations
+  for(s in 1:S){
+    rwm_step <- mh_sampling_step_cpp(x_curr = x[, s], l_curr = l_x,
+                                     l_target, ...,
+                                     sampler = mh_sampler, sampler_args = other_sampler_args,
+                                     lq_sampler = lq_mh, lq_sampler_args = other_lq_args)
+    x[, s+1] <- rwm_step$x_next
+    l_x <- rwm_step$l_next
+    acc[s] <- rwm_step$accepted
+  }
+
+  #--- Post processing and result -------------
+
+  acc_rate <- mean(acc)
+  if(!silent){
+    cat("Finished Sampling", sep = "\n")
+    cat(paste("Acceptance Rate:", round(acc_rate,3)), sep = "\n")
+  }
+
+  if(burn == -1){
+    return(list(x = x, l_x_curr = l_x, acc = acc, acc_rate = acc_rate))
+  }
+
+  if(burn == 0){
+    return(list(x = x[, -1], l_x_curr = l_x, acc = acc, acc_rate = acc_rate))
+  }
+
+  return(list(x = x[, -seq(1,burn+1)], l_x_curr = l_x, acc = acc[-seq(1,burn)], acc_rate = acc_rate))
+
+}
